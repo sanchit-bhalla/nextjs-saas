e24ee2bb2867485e533cede22e189be4e6c64073
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2025-08-07 00:44:35 +0530

    NextAuth setup with shadcn login form

---
+++ src/lib/auth.ts
@@ -0,0 +1,66 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credential-provider-id",
+      name: "Seth Traders",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials, req) {
+        console.log({ credentials });
+
+        // Any object returned will be saved in `user` property of the JWT
+        return {
+          id: "1",
+          name: credentials?.name,
+          email: credentials?.email,
+          password: credentials?.password,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name as string;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+      }
+      return session;
+    },
+  },
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+};
